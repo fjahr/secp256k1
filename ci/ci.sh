5f8445fbb4e3bfbb7bf47882b6981909278ab25c
Fabian Jahr <fjahr@protonmail.com>
2025-09-12 17:59:55 +0200

    fullagg: Add to CI

--- ci/ci.sh
+++ ci/ci.sh
@@ -13,7 +13,7 @@ print_environment() {
     # does not rely on bash.
     for var in WERROR_CFLAGS MAKEFLAGS BUILD \
             ECMULTWINDOW ECMULTGENKB ASM WIDEMUL WITH_VALGRIND EXTRAFLAGS \
-            EXPERIMENTAL ECDH RECOVERY EXTRAKEYS MUSIG SCHNORRSIG SCHNORRSIG_HALFAGG ELLSWIFT BATCH \
+            EXPERIMENTAL ECDH RECOVERY EXTRAKEYS MUSIG SCHNORRSIG SCHNORRSIG_HALFAGG SCHNORRSIG_FULLAGG ELLSWIFT BATCH \
             SECP256K1_TEST_ITERS BENCH SECP256K1_BENCH_ITERS CTIMETESTS SYMBOL_CHECK \
             EXAMPLES \
             HOST WRAPPER_CMD \
@@ -64,6 +64,7 @@ fi
     --enable-module-extrakeys="$EXTRAKEYS" \
     --enable-module-schnorrsig="$SCHNORRSIG" \
     --enable-module-schnorrsig-halfagg="$SCHNORRSIG_HALFAGG" \
+    --enable-module-schnorrsig-fullagg="$SCHNORRSIG_FULLAGG" \
     --enable-module-musig="$MUSIG" \
     --enable-module-batch="$BATCH" \
     --enable-examples="$EXAMPLES" \
